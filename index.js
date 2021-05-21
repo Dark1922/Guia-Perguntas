45140a7fa5bfe7199cd03c53d4c4f29508cb4d1a
dark1922 <jvdematos004@gmail.com>
2021-05-21 18:00:42 -0300

    feat: navbar , form

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const app = express();
 
 app.set('view engine','ejs'); //dizendo que a view engine motor de html que quero ultilizar usar o ejs como view engine.
-
+app.use(express.static('public')) //página estatica arquivos estaticos css js front end img etc
 app.get('/',(req, res) => {
-  res.render('index') //render desenhar algo na tela index da pasta views
+  res.render('index');
+});
+app.get('/perguntar', (req, res) => {
+   res.render('perguntar'); //renderize a views perguntar.ejs
 });
 
 app.listen(3333,() => {
